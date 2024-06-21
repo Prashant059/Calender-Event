207bd6cc5c0db4692deddcde6e63cfc38e54d7b8
Prashant Randive <prashant.randhive05@gmail.com>
2024-06-21 19:46:57 +0530

    first committ

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calender from './components/Calender/Calender'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Calender />
+    </>
+  )
+}
+
+export default App
